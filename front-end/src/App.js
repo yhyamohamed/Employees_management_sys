9d7f173c4b5f165f03d5c3b6e6b012363d825c80
HassanTawfic <hatawfic@gmail.com>
2022-07-19 02:37:10 +0200

    Departments

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,28 +1,28 @@
 import "./App.css";
-import { Routes, Route } from "react-router-dom";
+import {Routes, Route} from "react-router-dom";
 import Hello from "./components/Hello";
 import Navbar from "./components/Navbar"
 import ListAllUsers from "./components/users/ListAllUsers";
 import ListAllTasks from "./components/tasks/ListAllTasks";
 import ListAllVacations from "./components/vacations/ListAllVacations";
-
+import ListAllDepartments from "./components/departments/ListAllDepartments";
 
 
 function App() {
-  return (
-    <>
-      <Navbar />
-      <div className="container mt-5 bg-light p-1">
-        <Routes>
-          <Route exact path="/" element={<Hello />}></Route>
-          <Route  path="/users" element={<ListAllUsers />}></Route>
-          <Route  path="/tasks" element={<ListAllTasks />}></Route>
-          <Route  path="/vacations" element={<ListAllVacations />}></Route>
-
-        </Routes>
-      </div>
-    </>
-  );
+    return (
+        <>
+            <Navbar/>
+            <div className="container mt-5 bg-light p-1">
+                <Routes>
+                    <Route exact path="/" element={<Hello/>}></Route>
+                    <Route path="/users" element={<ListAllUsers/>}></Route>
+                    <Route path="/tasks" element={<ListAllTasks/>}></Route>
+                    <Route path="/vacations" element={<ListAllVacations/>}></Route>
+                    <Route path="/departments" element={<ListAllDepartments/>}></Route>
+                </Routes>
+            </div>
+        </>
+    );
 }
 
 export default App;
---
+++ front-end/src/components/departments/ListAllDepartments.js
@@ -0,0 +1,135 @@
+import {useState} from 'react';
+import useGet from "../../custumHooks/useGet";
+import DataTable from "react-data-table-component";
+// import React from "@types/react";
+
+
+const columns = [
+    {
+        name: 'ID',
+        selector: row => row.id,
+        sortable:true
+    },
+    {
+        name: 'Name',
+        selector: row => row.name,
+        sortable:true
+    },
+    {
+        name: 'Manager ID',
+        selector: row => row.manager_id,
+        sortable:true
+    },
+    {
+        name: 'Manager Started At',
+        selector: row => row.manager_start_at,
+        sortable:true
+    },
+    {
+        key: "action",
+        text: "Action",
+        className: "action",
+        width: 100,
+        align: "left",
+        sortable: false,
+        cell: (record) => {
+            return (
+                <>
+                    <button
+                        className="btn btn-sm btn-success"
+                        onClick={() => console.log(record)}
+                    >
+                        Edit
+                    </button>
+                </>
+            );
+        },
+    }
+];
+
+const customStyles = {
+    columns: {
+        style: {
+            width: "fit-content",
+        },
+    },
+    rows: {
+        style: {
+            minHeight: "50px", // override the row height
+
+        },
+    },
+    headCells: {
+        style: {
+            paddingLeft: "8px", // override the cell padding for head cells
+            paddingRight: "8px",
+
+        },
+    },
+    cells: {
+        style: {
+            paddingLeft: "8px", // override the cell padding for data cells
+            paddingRight: "8px",
+
+        },
+    },
+};
+
+const ListAllDepartments = () => {
+    const { data, isPending, error } = useGet("http://127.0.0.1:8000/api/departments");
+    const [txt, setTxt] = useState("");
+
+    function search(rows) {
+        return rows.filter((row) =>
+            row.name.toLowerCase().includes(txt.toLowerCase())
+        );
+    }
+
+    return (
+        <div className="row">
+            {isPending && (
+                <>
+                    <div className={'d-flex justify-content-center'}>
+                    <div className="alert alert-primary align-self-center" role="alert">
+            <span
+                className="spinner-border text-info spinner-border-m me-2"
+                role="status"
+            >
+              <span className="visually-hidden text-center">Loading...</span>
+            </span>
+                        <span className={'fs-5'}>Getting Data pleas wait!</span>
+                    </div>
+                    </div>
+                </>
+            )}
+            {error && (
+                <>
+                    <div className="alert alert-danger" role="alert">
+                        we'r sry , {error} ..pls try again later!
+                    </div>
+                </>
+            )}
+            {data && (
+                <>
+                    <div className="float-right row">
+                        <input
+                            className="form-control  col offset-8"
+                            type="text"
+                            placeholder="type to search"
+                            value={txt}
+                            onChange={(e) => setTxt(e.target.value)}
+                        />
+                    </div>
+                    <DataTable
+                        columns={columns}
+                        data={search(data)}
+                        pagination
+                        customStyles={customStyles}
+                    />
+                </>
+            )}
+        </div>
+    );
+};
+
+export default ListAllDepartments
