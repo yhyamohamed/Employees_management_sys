63c78a3ee55eb6e6eb8e3398f9fc29358d8fc52a
HassanTawfic <hatawfic@gmail.com>
2022-07-20 19:31:24 +0200

    View User Component

--- front-end/src/components/vacations/ListAllVacations.js
+++ front-end/src/components/vacations/ListAllVacations.js
@@ -1,210 +1,230 @@
 import useGet from "../../custumHooks/useGet";
-import { Link } from "react-router-dom";
+import {Link} from "react-router-dom";
 import DataTable from "react-data-table-component";
-import React, { useEffect, useState } from "react";
+import React, {useEffect, useState} from "react";
 import CreateVacationModal from "./CreateVacationModal";
 import DeleteVacationModal from "./DeleteVacationModal";
+import ViewVacation from "./ViewVacation";
 
 const customStyles = {
-  columns: {
-    style: {
-      width: "fit-content",
+    columns: {
+        style: {
+            width: "fit-content",
+        },
     },
-  },
-  rows: {
-    style: {
-      minHeight: "50px", // override the row height
+    rows: {
+        style: {
+            minHeight: "50px", // override the row height
+        },
     },
-  },
-  headCells: {
-    style: {
-      paddingLeft: "8px", // override the cell padding for head cells
-      paddingRight: "8px",
+    headCells: {
+        style: {
+            paddingLeft: "8px", // override the cell padding for head cells
+            paddingRight: "8px",
+        },
     },
-  },
-  cells: {
-    style: {
-      paddingLeft: "8px", // override the cell padding for data cells
-      paddingRight: "8px",
+    cells: {
+        style: {
+            paddingLeft: "8px", // override the cell padding for data cells
+            paddingRight: "8px",
+        },
     },
-  },
 };
 
 const ListAllVacations = () => {
-  const [txt, setTxt] = useState("");
-  const [success, setSuccess] = useState('');
-  const [currentID, setCurrentID] = useState(0);
+    const [txt, setTxt] = useState("");
+    const [success, setSuccess] = useState('');
+    const [currentID, setCurrentID] = useState(0);
+    const [viewVacationData, setVacationData] = useState({
+        user : {
+            name : '',
+            employee_title : '',
+            phone: '',
+            email: '',
+        },
+        department:{
+            name:'',
+        },
+        status: '',
+        reasons: '',
+        start_date:'',
+        duration:'',
 
-  const { data, isPending, error } = useGet(
-    "GET","http://127.0.0.1:8000/api/vacations"
-  );
-  
 
-  function search(rows) {
-    return rows.filter((row) =>
-      row.user.name.toLowerCase().includes(txt.toLowerCase())
+    })
+
+    const {data, isPending, error} = useGet(
+        "GET", "http://127.0.0.1:8000/api/vacations"
     );
-  }
 
-  const columns = [
-    {
-      name: "name",
-      selector: (row) => row.user.name,
-      sortable: true,
-    },
-    {
-      name: "Department",
-      selector: (row) => row.department.name,
-    },
-    {
-      name: "Duration",
-      selector: (row) => row.duration,
-    },
-    {
-      name: "Reasons",
-      selector: (row) => row.reasons,
-      sortable: true,
-    },
-    {
-      name: "Paid",
-      selector: (row) => row.paid?"YES":"NO",
-      width: "fit-content",
-    },
-    {
-      name: "Start Date",
-      selector: (row) => row.start_date,
-      sortable: true,
-    },
-    {
-      name: "End Date",
-      selector: (row) => row.end_date,
-      sortable: true,
-    },
-    {
-      name: "Status",
-      selector: (row) => row.status,
-      sortable: true,
-      width: "fit-content",
-    },
-    {
-      key: "action",
-      text: "Action",
-      name: "Action",
-      className: "action",
-      width: "8%",
-      align: "left",
-      sortable: false,
-  
-      cell: (record) => {
-        return (
-          <>
-            <div className="d-flex justify-content-between w-75">
-              <i
-                className="far fa-edit fa-lg me-2"
-                style={{ cursor: "pointer", color: "blue" }}
-                onClick={EditVacation}
-              ></i>
-              <i
-                className="fa-regular fa-trash-can fa-lg me-2"
-                style={{ cursor: "pointer", color: "red" }}
-                data-bs-toggle="modal"
-                data-bs-target="#deleteModal"
-                onClick={() => setCurrentID(record.id)}
-              ></i>
-  
-              <i
-                className="fa-solid fa-circle-info fa-lg me-2"
-                style={{ cursor: "pointer", color: "green" }}
-                onClick={() => console.log(record.id)}
-              ></i>
-            </div>
-          </>
+
+    function search(rows) {
+        return rows.filter((row) =>
+            row.user.name.toLowerCase().includes(txt.toLowerCase())
         );
-      },
-    },
-  ];
-  
-  return (
-    <div className="row">
-      {isPending && (
-        <>
-          <div className="alert alert-primary" role="alert">
+    }
+
+    const columns = [
+        {
+            name: "name",
+            selector: (row) => row.user.name,
+            sortable: true,
+        },
+        {
+            name: "Department",
+            selector: (row) => row.department.name,
+        },
+        {
+            name: "Duration",
+            selector: (row) => row.duration,
+        },
+        {
+            name: "Reasons",
+            selector: (row) => row.reasons,
+            sortable: true,
+        },
+        {
+            name: "Paid",
+            selector: (row) => row.paid ? "YES" : "NO",
+            width: "fit-content",
+        },
+        {
+            name: "Start Date",
+            selector: (row) => row.start_date,
+            sortable: true,
+        },
+        {
+            name: "End Date",
+            selector: (row) => row.end_date,
+            sortable: true,
+        },
+        {
+            name: "Status",
+            selector: (row) => row.status,
+            sortable: true,
+            width: "fit-content",
+        },
+        {
+            key: "action",
+            text: "Action",
+            name: "Action",
+            className: "action",
+            width: "8%",
+            align: "left",
+            sortable: false,
+
+            cell: (record) => {
+                return (
+                    <>
+                        <div className="d-flex justify-content-between w-75">
+                            <i
+                                className="far fa-edit fa-lg me-2"
+                                style={{cursor: "pointer", color: "blue"}}
+                                onClick={() => {
+                                    console.log(record)
+                                }}
+                            ></i>
+                            <i
+                                className="fa-regular fa-trash-can fa-lg me-2"
+                                style={{cursor: "pointer", color: "red"}}
+                                data-bs-toggle="modal"
+                                data-bs-target="#deleteModal"
+                                onClick={() => setCurrentID(record.id)}
+                            ></i>
+
+                            <i
+                                className="fa-solid fa-circle-info fa-lg me-2"
+                                style={{cursor: "pointer", color: "green"}}
+                                data-bs-toggle="modal"
+                                data-bs-target="#viewModal"
+                                onClick={() => setVacationData(record)}
+                            ></i>
+                        </div>
+                    </>
+                );
+            },
+        },
+    ];
+
+    return (
+        <div className="row">
+            {isPending && (
+                <>
+                    <div className="alert alert-primary" role="alert">
             <span
-              className="spinner-border text-info spinner-border-m me-2"
-              role="status"
+                className="spinner-border text-info spinner-border-m me-2"
+                role="status"
             >
               <span className="visually-hidden">Loading...</span>
             </span>
-            Getting Data pleas wait!
-          </div>
-        </>
-      )}
-      {error && (
-        <>
-          <div className="alert alert-danger" role="alert">
-            we'r sry , {error} ..pls try again later!
-          </div>
-        </>
-      )}
-      {data && (
-        <>
-          <div className="row ">
-          {success && (
-              <div
-                className="alert alert-success alert-dismissible fade show"
-                role="alert"
-              >
-                {success}
-                <button
-                  onClick={() => setSuccess("")}
-                  type="button"
-                  className="btn-close"
-                  data-bs-dismiss="alert"
-                  aria-label="Close"
-                ></button>
-              </div>
+                        Getting Data pleas wait!
+                    </div>
+                </>
             )}
-            <div className="offset-6 col-3 input-group-sm ">
-              <input
-                className="form-control "
-                type="text"
-                placeholder="Search by name"
-                value={txt}
-                onChange={(e) => setTxt(e.target.value)}
-              />
-            </div>
-            <div className=" col-2 me-1 ">
-              <button
-                className="btn btn-sm btn-success "
-                data-bs-toggle="modal"
-                data-bs-target="#modal"
-              >
-                Add Vacation
-              </button>
-            </div>
-            <CreateVacationModal
-              setSuccess={setSuccess}
-            />
-            <DeleteVacationModal
-            id={currentID}
-            setSuccess={setSuccess}
-            />
-            </div>
-          <DataTable
-            columns={columns}
-            data={search(data)}
-            pagination
-            customStyles={customStyles}
-          />
-        </>
-      )}
-    </div>
-  );
+            {error && (
+                <>
+                    <div className="alert alert-danger" role="alert">
+                        we'r sry , {error} ..pls try again later!
+                    </div>
+                </>
+            )}
+            {data && (
+                <>
+                    <div className="row ">
+                        {success && (
+                            <div
+                                className="alert alert-success alert-dismissible fade show"
+                                role="alert"
+                            >
+                                {success}
+                                <button
+                                    onClick={() => setSuccess("")}
+                                    type="button"
+                                    className="btn-close"
+                                    data-bs-dismiss="alert"
+                                    aria-label="Close"
+                                ></button>
+                            </div>
+                        )}
+                        <div className="offset-6 col-3 input-group-sm ">
+                            <input
+                                className="form-control "
+                                type="text"
+                                placeholder="Search by name"
+                                value={txt}
+                                onChange={(e) => setTxt(e.target.value)}
+                            />
+                        </div>
+                        <div className=" col-2 me-1 ">
+                            <button
+                                className="btn btn-sm btn-success "
+                                data-bs-toggle="modal"
+                                data-bs-target="#modal"
+                            >
+                                Add Vacation
+                            </button>
+                        </div>
+                        <CreateVacationModal
+                            setSuccess={setSuccess}
+                        />
+                        <DeleteVacationModal
+                            id={currentID}
+                            setSuccess={setSuccess}
+                        />
+                        <ViewVacation
+                            record={viewVacationData}
+                        />
+                    </div>
+                    <DataTable
+                        columns={columns}
+                        data={search(data)}
+                        pagination
+                        customStyles={customStyles}
+                    />
+                </>
+            )}
+        </div>
+    );
 };
-
-const EditVacation = () => {
-  
-  console.log("tmam")
-}
 
 export default ListAllVacations;
