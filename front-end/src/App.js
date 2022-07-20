0f2d6bbd1757fc168ad3573d7690231c483abdb5
yhyamohamed <yhyashehata1@gmail.com>
2022-07-20 18:13:45 +0200

    login form

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,28 +1,19 @@
 import "./App.css";
-import {Routes, Route} from "react-router-dom";
-import Hello from "./components/Hello";
-import Navbar from "./components/Navbar"
-import ListAllUsers from "./components/users/ListAllUsers";
-import ListAllTasks from "./components/tasks/ListAllTasks";
-import ListAllVacations from "./components/vacations/ListAllVacations";
-import ListAllDepartments from "./components/departments/ListAllDepartments";
+import { Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
 
+
+import MainRoutes from "./MainRoutes";
 
 function App() {
-    return (
-        <>
-            <Navbar/>
-            <div className="container mt-5 bg-light p-1">
-                <Routes>
-                    <Route exact path="/" element={<Hello/>}></Route>
-                    <Route path="/users" element={<ListAllUsers/>}></Route>
-                    <Route path="/tasks" element={<ListAllTasks/>}></Route>
-                    <Route path="/vacations" element={<ListAllVacations/>}></Route>
-                    <Route path="/departments" element={<ListAllDepartments/>}></Route>
-                </Routes>
-            </div>
-        </>
-    );
+  return (
+    <>
+      <Routes>
+        <Route exact path="/" element={<Home />}></Route>
+        <Route  path="*" element={<MainRoutes/>}></Route>
+      </Routes>
+    </>
+  );
 }
 
 export default App;
