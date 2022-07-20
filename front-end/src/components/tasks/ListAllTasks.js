989b9281b53f1bdb070d3c01839c26877dc4e3ad
Mohamed <darkmorning98@gmail.com>
2022-07-20 14:46:53 +0200

    Delete functionality and modal

--- front-end/src/components/tasks/ListAllTasks.js
+++ front-end/src/components/tasks/ListAllTasks.js
@@ -2,86 +2,11 @@ import { useEffect, useState } from "react";
 import useGet from "../../custumHooks/useGet";
 import DataTable from "react-data-table-component";
 import CreateTaskModal from "./CreateTaskModal";
-
-const columns = [
-  {
-    name: "ID",
-    selector: (row) => row.id,
-    sortable: true,
-  },
-  {
-    name: "Name",
-    selector: (row) => row.name,
-    sortable: true,
-  },
-  {
-    name: "Code",
-    selector: (row) => row.code,
-    sortable: true,
-  },
-  {
-    name: "created At",
-    selector: (row) => row.created_at,
-    sortable: true,
-  },
-  {
-    name: "Description",
-    selector: (row) => row.description,
-    sortable: true,
-  },
-  {
-    name: "Due Date",
-    selector: (row) => row.due_date,
-    sortable: true,
-  },
-  {
-    name: "Priority",
-    selector: (row) => row.priority,
-    sortable: true,
-  },
-  {
-    name: "Status",
-    selector: (row) => row.status,
-    sortable: true,
-  },
-  {
-    key: "action",
-    text: "Action",
-    name: "Action",
-    className: "action",
-    width: "8%",
-    align: "left",
-    sortable: false,
-
-    cell: (record) => {
-      return (
-          <>
-            <div className="d-flex justify-content-between w-75">
-              <i
-                  className="far fa-edit fa-lg me-2"
-                  style={{ cursor: "pointer", color: "blue" }}
-                  onClick={() => console.log(record.id)}
-              ></i>
-              <i
-                  className="fa-regular fa-trash-can fa-lg me-2"
-                  style={{ cursor: "pointer", color: "red" }}
-                  onClick={() => console.log(record.id)}
-              ></i>
-
-              <i
-                  className="fa-solid fa-circle-info fa-lg me-2"
-                  style={{ cursor: "pointer", color: "green" }}
-                  onClick={() => console.log(record.id)}
-              ></i>
-            </div>
-          </>
-      );
-    },
-  },
-];
+import DeleteTaskModal from "./DeleteTaskModal";
 
 function ListAllTasks() {
     const [txt, setTxt] = useState("");
+    const [currentID, setCurrentID] = useState(0);
     const [success, setSuccess] = useState('');
     const { data, isPending, error } = useGet(
       "GET",
@@ -93,6 +18,85 @@ function ListAllTasks() {
       row.name.toLowerCase().includes(txt.toLowerCase())
     );
   }
+
+  const columns = [
+    {
+      name: "ID",
+      selector: (row) => row.id,
+      sortable: true,
+    },
+    {
+      name: "Name",
+      selector: (row) => row.name,
+      sortable: true,
+    },
+    {
+      name: "Code",
+      selector: (row) => row.code,
+      sortable: true,
+    },
+    {
+      name: "created At",
+      selector: (row) => row.created_at,
+      sortable: true,
+    },
+    {
+      name: "Description",
+      selector: (row) => row.description,
+      sortable: true,
+    },
+    {
+      name: "Due Date",
+      selector: (row) => row.due_date,
+      sortable: true,
+    },
+    {
+      name: "Priority",
+      selector: (row) => row.priority,
+      sortable: true,
+    },
+    {
+      name: "Status",
+      selector: (row) => row.status,
+      sortable: true,
+    },
+    {
+      key: "action",
+      text: "Action",
+      name: "Action",
+      className: "action",
+      width: "8%",
+      align: "left",
+      sortable: false,
+
+      cell: (record) => {
+        return (
+            <>
+              <div className="d-flex justify-content-between w-75">
+                <i
+                    className="far fa-edit fa-lg me-2"
+                    style={{ cursor: "pointer", color: "blue" }}
+                    onClick={() => console.log(record.id)}
+                ></i>
+                <i
+                    className="fa-regular fa-trash-can fa-lg me-2"
+                    style={{ cursor: "pointer", color: "red" }}
+                    data-bs-toggle="modal"
+                    data-bs-target="#deleteModal"
+                    onClick={() => setCurrentID(record.id)}
+                ></i>
+
+                <i
+                    className="fa-solid fa-circle-info fa-lg me-2"
+                    style={{ cursor: "pointer", color: "green" }}
+                    onClick={() => console.log(record.id)}
+                ></i>
+              </div>
+            </>
+        );
+      },
+    },
+  ];
 
   return (
     <div className="row">
@@ -155,6 +159,10 @@ function ListAllTasks() {
             </div>
             <CreateTaskModal
               setSuccess={setSuccess}
+            />
+            <DeleteTaskModal
+                id={currentID}
+                setSuccess={setSuccess}
             />
           </div>
           <DataTable columns={columns} data={search(data)} pagination />
