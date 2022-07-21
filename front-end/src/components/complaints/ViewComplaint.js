40911fb5b58bafe1a2bfe2ed829ee67536c0bccc
Mahmoud Mohamed Kamal <54429800+MahmoudFierro98@users.noreply.github.com>
2022-07-21 14:39:37 +0200

    Fix Bugs

--- front-end/src/components/complaints/ViewComplaint.js
+++ front-end/src/components/complaints/ViewComplaint.js
@@ -16,8 +16,9 @@ function ViewComplaint({record}) {
                             <div>
                                 {record.subject !== null && (
                                     <div>
-                                        <p><span className={'fw-bold'}>Employee: </span>{record.user.name}</p>
-                                        <p><span className={'fw-bold'}>Department: </span>{record.department.name}</p>
+                                        {/* Bug here */}
+                                        {/* <p><span className={'fw-bold'}>Employee: </span>{record.user.name}</p>
+                                        <p><span className={'fw-bold'}>Department: </span>{record.department.name}</p> */}
                                         <p><span className={'fw-bold'}>Subject: </span>{record.subject}</p>
                                         <p><span className={'fw-bold'}>Body: </span>{record.body}</p>
                                         <p><span className={'fw-bold'}>Reasons: </span>{record.reasons}</p>
