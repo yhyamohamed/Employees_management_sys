0d588be700985c7db4c326a0219e44057daf6a8f
HassanTawfic <hatawfic@gmail.com>
2022-07-23 20:15:39 +0200

    adjusting policies

--- back-end/app/Providers/AuthServiceProvider.php
+++ back-end/app/Providers/AuthServiceProvider.php
@@ -2,7 +2,16 @@
 
 namespace App\Providers;
 
+use App\Models\Attendance;
+use App\Models\Complaint;
+use App\Models\Leave;
+use App\Models\OverTime;
+use App\Models\Task;
 use App\Models\Vacation;
+use App\Policies\AttendancePolicy;
+use App\Policies\ComplaintPolicy;
+use App\Policies\LeavePolicy;
+use App\Policies\OverTimePolicy;
 use App\Policies\VacationPolicy;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Gate;
@@ -17,6 +26,11 @@ class AuthServiceProvider extends ServiceProvider
     protected $policies = [
         // 'App\Models\Model' => 'App\Policies\ModelPolicy',
         Vacation::class=>VacationPolicy::class,
+        Attendance::class=>VacationPolicy::class,
+        Complaint::class=>VacationPolicy::class,
+        Leave::class=>VacationPolicy::class,
+        OverTime::class=>VacationPolicy::class,
+        Task::class=>VacationPolicy::class
     ];
 
     /**
