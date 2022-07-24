962283a85d7a1fcc2109ab79bd41d34e307a432b
yhyamohamed <yhyashehata1@gmail.com>
2022-07-24 11:34:43 +0200

    attendance and leave bugfix

--- front-end/src/components/Login.js
+++ front-end/src/components/Login.js
@@ -1,124 +1,134 @@
-import {useContext, useEffect, useState} from "react";
+import { useContext, useEffect, useState } from "react";
 import APIService from "../services/APIService";
-import {useNavigate} from "react-router-dom";
-import {UserContext} from "../App";
-import logo from '../assets/beehive-logo.png';
-
+import { useNavigate } from "react-router-dom";
+import { UserContext } from "../App";
+import logo from "../assets/beehive-logo.png";
 
 const Login = () => {
-    const [data, setData] = useState({
-        email: '',
-        password: '',
-    });
-    const [success, setSuccess] = useState('');
-    const [error, setError] = useState('');
-    const [loading, setLoading] = useState(false);
+  const [data, setData] = useState({
+    email: "",
+    password: "",
+  });
+  const [success, setSuccess] = useState("");
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState(false);
 
-    const {user, setUser} = useContext(UserContext);
+  const { user, setUser } = useContext(UserContext);
 
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        setLoading(true);
-        const result = await APIService.post('http://127.0.0.1:8000/api/login', data);
-        if (result.success) {
-            setSuccess('Login successful.');
-            console.log(result.result.data.token);
-            const user = {
-                authenticated: true,
-                token: result.result.data.token,
-                ...result.result.data
-            }
-            await APIService.post('http://127.0.0.1:8000/api/attendance', {
-                user_id: user.id,
-            });
-            setUser(user);
-            localStorage.setItem('token', result.result.data.token);
-            navigate('/home');
-        } else {
-            setError(result.error);
-        }
-        setLoading(false);
-    }
-
-    useEffect(() => {
-        if (user.authenticated) {
-            navigate('/home');
-        }
-    }, [user]);
-    return (
-        <>
-            <div className="vh-100 d-flex justify-content-center align-items-center gap-5">
-                <div className="p-2 d-flex flex-column justify-content-center align-items-center">
-                    {/*<i*/}
-                    {/*    className="fa-solid fa-feather-pointed fa-10x"*/}
-                    {/*    style={{color: "white"}}*/}
-                    {/*></i>*/}
-                    <img src={logo} width={200} alt="logo"/>
-                    {/*<br/>*/}
-                    <h1 className="mt-1 text-white">BEEHIVE</h1>
-                </div>
-                <div className="border-end border-white border-2 h-25"></div>
-                <div className=" flex items-center justify-center main">
-                    <form onSubmit={handleSubmit} className=" w-2/4 pr-4 login ">
-                        <h1 className="text-xl italic font-medium mb-2 text-white">Login</h1>
-
-
-                        <div className="form-group">
-                            <label htmlFor="exampleInputEmail1" className="text-white">Email</label>
-                            <input
-                                type="email"
-                                className="form-control"
-                                aria-describedby="emailHelp"
-                                placeholder="Enter email"
-                                onChange={(e) => setData({...data, email: e.target.value})}
-                            />
-
-                        </div>
-                        <div className="form-group">
-                            <label htmlFor="exampleInputPassword1" className="text-white">Password</label>
-                            <input
-                                type="password"
-                                className="form-control"
-                                placeholder="Password"
-                                onChange={(e) => setData({...data, password: e.target.value})}
-                            />
-                        </div>
-
-
-                        <button
-                            type="submit"
-                            className="btn btn-primary btn-block btn-large focus:outline-none hover:bg-blue-500 mt-2"
-                            disabled={loading}
-                        >
-                            Log Me In
-                        </button>
-                        {error && (
-                            <div className="alert alert-danger" role="alert">
-                                <small>{error}</small>
-                            </div>
-                        )}
-                        {success && (
-                            <div
-                                className="alert alert-success alert-dismissible fade show"
-                                role="alert"
-                            >
-                                {success}
-                                <button
-                                    onClick={() => setSuccess("")}
-                                    type="button"
-                                    className="btn-close"
-                                    data-bs-dismiss="alert"
-                                    aria-label="Close"
-                                ></button>
-                            </div>)}
-                    </form>
-                    {/* <div className="flex-2 logo pl-4"><i className="fas fa-feather-alt"></i> LOGO</div> */}
-                </div>
-            </div>
-        </>
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+    const result = await APIService.post(
+      "http://127.0.0.1:8000/api/login",
+      data
     );
+    if (result.success) {
+      setSuccess("Login successful.");
+      console.log(result.result.data.token);
+      const user = {
+        authenticated: true,
+        token: result.result.data.token,
+        ...result.result.data,
+      };
+      await APIService.post(
+        "http://127.0.0.1:8000/api/attendance",
+        {
+          user_id: user.id,
+        },
+        result.result.data.token
+      );
+      setUser(user);
+      localStorage.setItem("token", result.result.data.token);
+      navigate("/home");
+    } else {
+      setError(result.error);
+    }
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    if (user.authenticated) {
+      navigate("/home");
+    }
+  }, [user]);
+  return (
+    <>
+      <div className="vh-100 d-flex justify-content-center align-items-center gap-5">
+        <div className="p-2 d-flex flex-column justify-content-center align-items-center">
+          {/*<i*/}
+          {/*    className="fa-solid fa-feather-pointed fa-10x"*/}
+          {/*    style={{color: "white"}}*/}
+          {/*></i>*/}
+          <img src={logo} width={200} alt="logo" />
+          {/*<br/>*/}
+          <h1 className="mt-1 text-white">BEEHIVE</h1>
+        </div>
+        <div className="border-end border-white border-2 h-25"></div>
+        <div className=" flex items-center justify-center main">
+          <form onSubmit={handleSubmit} className=" w-2/4 pr-4 login ">
+            <h1 className="text-xl italic font-medium mb-2 text-white">
+              Login
+            </h1>
+
+            <div className="form-group">
+              <label htmlFor="exampleInputEmail1" className="text-white">
+                Email
+              </label>
+              <input
+                type="email"
+                className="form-control"
+                aria-describedby="emailHelp"
+                placeholder="Enter email"
+                onChange={(e) => setData({ ...data, email: e.target.value })}
+              />
+            </div>
+            <div className="form-group">
+              <label htmlFor="exampleInputPassword1" className="text-white">
+                Password
+              </label>
+              <input
+                type="password"
+                className="form-control"
+                placeholder="Password"
+                onChange={(e) => setData({ ...data, password: e.target.value })}
+              />
+            </div>
+
+            <button
+              type="submit"
+              className="btn btn-primary btn-block btn-large focus:outline-none hover:bg-blue-500 mt-2"
+              disabled={loading}
+            >
+              Log Me In
+            </button>
+            {error && (
+              <div className="alert alert-danger" role="alert">
+                <small>{error}</small>
+              </div>
+            )}
+            {success && (
+              <div
+                className="alert alert-success alert-dismissible fade show"
+                role="alert"
+              >
+                {success}
+                <button
+                  onClick={() => setSuccess("")}
+                  type="button"
+                  className="btn-close"
+                  data-bs-dismiss="alert"
+                  aria-label="Close"
+                ></button>
+              </div>
+            )}
+          </form>
+          {/* <div className="flex-2 logo pl-4"><i className="fas fa-feather-alt"></i> LOGO</div> */}
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Login;
--- front-end/src/components/attendance/ListAllAtendance.js
+++ front-end/src/components/attendance/ListAllAtendance.js
@@ -1,188 +1,189 @@
-import React from 'react';
-import {useContext, useEffect, useState} from "react";
+import React from "react";
+import { useContext, useEffect, useState } from "react";
 import useGet from "../../custumHooks/useGet";
-import {useNavigate} from "react-router-dom";
-import {UserContext} from "../../App";
+import { useNavigate } from "react-router-dom";
+import { UserContext } from "../../App";
 import DataTable from "react-data-table-component";
 import ViewAttendance from "./ViewAttendance";
 
 const customStyles = {
-    columns: {
-        style: {
-            width: "fit-content",
-        },
+  columns: {
+    style: {
+      width: "fit-content",
     },
-    rows: {
-        style: {
-            minHeight: "50px", // override the row height
-        },
+  },
+  rows: {
+    style: {
+      minHeight: "50px", // override the row height
     },
-    headCells: {
-        style: {
-            paddingLeft: "8px", // override the cell padding for head cells
-            paddingRight: "8px",
-        },
+  },
+  headCells: {
+    style: {
+      paddingLeft: "8px", // override the cell padding for head cells
+      paddingRight: "8px",
     },
-    cells: {
-        style: {
-            paddingLeft: "8px", // override the cell padding for data cells
-            paddingRight: "8px",
-        },
+  },
+  cells: {
+    style: {
+      paddingLeft: "8px", // override the cell padding for data cells
+      paddingRight: "8px",
     },
+  },
 };
 
 function ListAllAttendance() {
-    const [txt, setTxt] = useState("");
-    const [attendanceData, setAttendanceData] = useState({
-        user:{
-            name:'',
-            employee_code:'',
-            hired_at:'',
-            gender:'',
-            phone:'',
-            email:'',
-            department:{
-                name:'',
-            },
-            supervisor:{
-                name:'',
-                phone:'',
-                email:'',
-            }
-        }
-    })
+  const [txt, setTxt] = useState("");
+  const [attendanceData, setAttendanceData] = useState({
+    user: {
+      name: "",
+      employee_code: "",
+      hired_at: "",
+      gender: "",
+      phone: "",
+      email: "",
+      department: {
+        name: "",
+      },
+      supervisor: {
+        name: "",
+        phone: "",
+        email: "",
+      },
+    },
+  });
 
-    const {data, isPending, error} = useGet(
-        "GET", "http://127.0.0.1:8000/api/attendance", localStorage.getItem('token')
+  const { data, isPending, error } = useGet(
+    "GET",
+    "http://127.0.0.1:8000/api/attendance",
+    localStorage.getItem("token")
+  );
+
+  const navigate = useNavigate();
+
+  const { user } = useContext(UserContext);
+
+  useEffect(() => {
+    if (!user.authenticated) navigate("/login");
+    if (
+      user.employee_group !== "admin" &&
+      user.employee_group !== "higher-management" &&
+      user.employee_group !== "middle-management"
+    )
+      navigate("/home");
+  }, [user]);
+
+  function search(rows) {
+    return rows.filter((row) =>
+      row.user.name.toLowerCase().includes(txt.toLowerCase())
     );
+  }
 
-    const navigate = useNavigate();
+  const columns = [
+    {
+      name: "ID",
+      selector: (row) => row.id,
+      sortable: true,
+    },
+    {
+      name: "User ID",
+      selector: (row) => row.user_id,
+      sortable: true,
+    },
+    {
+      name: "Name",
+      selector: (row) => row.user.name,
+      sortable: true,
+    },
+    {
+      name: "Phone Number",
+      selector: (row) => row.user.phone,
+      sortable: true,
+    },
+    {
+      name: "Employee Group",
+      selector: (row) => row.user.employee_group,
+      sortable: true,
+    },
+    {
+      name: "Employee Title",
+      selector: (row) => row.user.employee_title,
+      sortable: true,
+    },
+    {
+      key: "action",
+      text: "Action",
+      name: "Action",
+      className: "action",
+      width: "8%",
+      align: "left",
+      sortable: false,
 
-    const {user} = useContext(UserContext);
-
-    useEffect(() => {
-        if (!user.authenticated)
-            navigate("/login");
-        if(user.employee_group !== 'admin' && user.employee_group !== 'higher-management' && user.employee_group !== 'middle-management')
-            navigate('/home');
-    }, [user]);
-
-
-    function search(rows) {
-        return rows.filter((row) =>
-            row.user.name.toLowerCase().includes(txt.toLowerCase())
+      cell: (record) => {
+        return (
+          <>
+            <div className="d-flex justify-content-evenly w-75">
+              <i
+                className="fa-solid fa-circle-info fa-lg me-2"
+                style={{ cursor: "pointer", color: "green" }}
+                data-bs-toggle="modal"
+                data-bs-target="#viewModal"
+                onClick={() => setAttendanceData(record)}
+              ></i>
+            </div>
+          </>
         );
-    }
+      },
+    },
+  ];
 
-    const columns = [
-        {
-            name: "Vacation ID",
-            selector: (row) => row.id,
-            sortable: true,
-        },
-        {
-            name: "User ID",
-            selector: (row) => row.user_id,
-            sortable: true,
-        },
-        {
-            name: "Name",
-            selector: (row) => row.user.name,
-            sortable: true,
-        },
-        {
-            name: "Phone Number",
-            selector: (row) => row.user.phone,
-            sortable: true,
-        },
-        {
-            name: "Employee Group",
-            selector: (row) => row.user.employee_group,
-            sortable: true,
-        },
-        {
-            name: "Employee Title",
-            selector: (row) => row.user.employee_title,
-            sortable: true,
-        },
-        {
-            key: "action",
-            text: "Action",
-            name: "Action",
-            className: "action",
-            width: "8%",
-            align: "left",
-            sortable: false,
-
-            cell: (record) => {
-                return (
-                    <>
-                        <div className="d-flex justify-content-evenly w-75">
-                            <i
-                                className="fa-solid fa-circle-info fa-lg me-2"
-                                style={{cursor: "pointer", color: "green"}}
-                                data-bs-toggle="modal"
-                                data-bs-target="#viewModal"
-                                onClick={() => setAttendanceData(record)}
-                            ></i>
-                        </div>
-                    </>
-                );
-            },
-        },
-    ];
-
-    return (
-        <div>
-            <div className="row">
-                {isPending && (
-                    <>
-                        <div className="alert alert-primary" role="alert">
-            <span
+  return (
+    <div>
+      <div className="row">
+        {isPending && (
+          <>
+            <div className="alert alert-primary" role="alert">
+              <span
                 className="spinner-border text-info spinner-border-m me-2"
                 role="status"
-            >
-              <span className="visually-hidden">Loading...</span>
-            </span>
-                            Getting Data pleas wait!
-                        </div>
-                    </>
-                )}
-                {error && (
-                    <>
-                        <div className="alert alert-danger" role="alert">
-                            we'r sry , {error} ..pls try again later!
-                        </div>
-                    </>
-                )}
-                {data && (
-                    <>
-                        <div className="row ">
-                            <div className="offset-6 col-3 input-group-sm mb-1">
-                                <input
-                                    className="form-control "
-                                    type="text"
-                                    placeholder="type to search"
-                                    value={txt}
-                                    onChange={(e) => setTxt(e.target.value)}
-                                />
-                            </div>
-                        </div>
-                        <ViewAttendance
-                            record={attendanceData}
-                        />
-                        <DataTable
-                            columns={columns}
-                            data={search(data)}
-                            pagination
-                            customStyles={customStyles}
-                        />
-                    </>
-                )}
-
-        </div>
-        </div>
-    );
+              >
+                <span className="visually-hidden">Loading...</span>
+              </span>
+              Getting Data pleas wait!
+            </div>
+          </>
+        )}
+        {error && (
+          <>
+            <div className="alert alert-danger" role="alert">
+              we'r sry , {error} ..pls try again later!
+            </div>
+          </>
+        )}
+        {data && (
+          <>
+            <div className="row ">
+              <div className="offset-6 col-3 input-group-sm mb-1">
+                <input
+                  className="form-control "
+                  type="text"
+                  placeholder="type to search"
+                  value={txt}
+                  onChange={(e) => setTxt(e.target.value)}
+                />
+              </div>
+            </div>
+            <ViewAttendance record={attendanceData} />
+            <DataTable
+              columns={columns}
+              data={search(data)}
+              pagination
+              customStyles={customStyles}
+            />
+          </>
+        )}
+      </div>
+    </div>
+  );
 }
 
 export default ListAllAttendance;
